26e69b498ee01dfe257d99c57d4f6fbb71928ea9
Yakov Putiliov <jack27@ya.ru>
2023-11-29 22:58:22 +0300

    Complite Task1

--- Task1 HW3/Program.cs
+++ Task1 HW3/Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿//Задача 1: Задайте одномерный массив из 10 целых чисел от 1 до 100. Найдите количество 
+// элементов массива, значения которых лежат в отрезке [20,90].
+int [] CreateArray (int min, int max, int size)
+{
+    int [] array = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        array [i] = new Random().Next (min, max + 1);
+    }
+    return array;
+}
+
+void ShowArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write ($"{array [i]} ");
+    }
+    Console.WriteLine ();
+} 
+
+int CountDigit (int [] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 20  && array[i]<= 90)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+int [] array = CreateArray (1, 100, 10);
+ShowArray (array);
+Console.WriteLine("Answer is " + CountDigit(array));
